0fad4f7b93a4af597f444933c0abfebbd67e64d5
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:17:16 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:17:12.930Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T13:17:17.377Z
+Commit: Fix memory leak in event handlers
