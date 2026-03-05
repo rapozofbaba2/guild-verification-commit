19154f7582d9224038e12012c528e6099057366e
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:13:21 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:13:22.062Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:13:23.076Z
+Commit: Fix CSS styling bug on mobile devices
