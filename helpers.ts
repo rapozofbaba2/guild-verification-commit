63462cbd0e08597109b131c628d15acaa8f0ad94
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:17:19 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:17:17.377Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T13:17:20.782Z
+Commit: Fix broken links in documentation
