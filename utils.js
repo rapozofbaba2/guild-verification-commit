65b4b610cc4fdc7602e1541876b9869ac53546a0
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:17:12 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:15:20.651Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T13:17:14.065Z
+Commit: Add export functionality for reports
