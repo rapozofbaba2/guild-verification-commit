93a3fe24531532f8fd66d3913acf64eec0186a03
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:28:25 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:17:08.492Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T13:28:26.659Z
+Commit: Fix broken links in documentation
