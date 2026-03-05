efadd0abd0ec98cd8800b9c92a46e83d069a36f5
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:08:55 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:08:40.466Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T13:08:56.327Z
+Commit: Resolve timezone issues in date handling
