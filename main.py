e3bcbbfd698dd2da2deb4cb0bc73520faf01ac40
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:17:06 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:15:08.401Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T13:17:07.502Z
+Commit: Fix memory leak in event handlers
