064374cdf1af64f0a1624817ca8e59483122341f
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:28:27 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:28:26.659Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T13:28:28.782Z
+Commit: Fix CSS styling bug on mobile devices
