8eb1aa20af88eab39e83eb189f8d440f6934f964
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:08:59 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:08:56.327Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T13:09:00.272Z
+Commit: Add multi-language support
