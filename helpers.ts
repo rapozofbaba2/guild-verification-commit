978bbad6059db833f3a1181660bed3d05f4b459e
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:09:53 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:09:51.235Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T13:09:54.366Z
+Commit: Fix CSS styling bug on mobile devices
