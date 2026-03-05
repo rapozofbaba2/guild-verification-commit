f30b54b862b4460e9efce6dd8d420ef4fb16267a
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:15:19 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:15:11.755Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T13:15:20.651Z
+Commit: Resolve null pointer exception in user service
