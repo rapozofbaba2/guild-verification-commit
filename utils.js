e25eaf403b1864cb2de05235a8dd48add44daee7
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:08:56 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:08:49.899Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T13:08:58.209Z
+Commit: Introduce real-time notifications
