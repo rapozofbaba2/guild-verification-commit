f9bd738c5d3e2b53164195f1bda032f8665d3fa8
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:08:48 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:08:09.203Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T13:08:49.899Z
+Commit: Resolve timezone issues in date handling
