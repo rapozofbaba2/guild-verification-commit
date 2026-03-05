961a1078a8252d6ffb7121ad3d4b2aa3c3a1f35c
rapozofbaba2 <rapozoftr@gmail.com>
2026-03-05 16:17:18 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:17:10.594Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T13:17:19.669Z
+Commit: Fix broken links in documentation
